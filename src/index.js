c32c017e38b2d846bd1366a528abe6914a93a8f4
Prometei299 <pavel-luchs@ya.ru>
2018-09-11 14:07:44 +0300

    LoveTriangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let arr = [0];
+  preferences = arr.concat(preferences);
+  let count = 0;
+
+  for(let i =1 ; i<= preferences.length; i++)
+  {
+    let first = preferences[i];
+    let second = preferences[first];
+    let third = preferences[second];
+
+    if((third == i) 
+    && preferences[first]!= first 
+    && preferences[second] != second 
+    && preferences[third] != third){
+      count ++
+    }
+  }
+    return count/3
 };
